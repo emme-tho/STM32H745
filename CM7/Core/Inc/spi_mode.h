43c3f52b94e65ceedee509769ff0b9de5df8fa97
Emmert <thomas.emmert@siemens-healthineers.com>
2026-01-09 15:07:03 +0100

    SPI: Setup erstellt

--- CM7/Core/Inc/spi_mode.h
+++ CM7/Core/Inc/spi_mode.h
@@ -1,15 +1,10 @@
-/*
- * spi_mode.h
- *
- *  Created on: Jan 9, 2026
- *      Author: emmethsg
- */
-
 #ifndef INC_SPI_MODE_H_
 #define INC_SPI_MODE_H_
 
+#include <stdint.h>
 
-
-
+void SPI_Mode_Enter(void);
+uint8_t SPI_Mode_HandleLine(char *line);
+uint8_t SPI_Mode_HandleChar(char ch);
 
 #endif /* INC_SPI_MODE_H_ */
